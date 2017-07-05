0097165a5f11cb85db5388d64d0cccf3fbd09b7e
yijing1998 <yijing1998@126.com>
2017-07-05 16:47:16 +0800

    can do things right
    no condition judgement, you need a clean working place!

--- recipe.sh
+++ recipe.sh
@@ -4,27 +4,81 @@
 # varity setting files, symbolic files to tie hexo platform with existing user 
 # files
 
-hfolder='hexofolder'
+rfolder=`pwd`
 tfolder='themes'
-: ${tlist:="
+: ${trepos:="
 	next|https://github.com/yijing1998.abc.git|branch
 	test|https://a.b.c|xyz
 "}
+ufolder='ufiles'
+urepo='urepo|https://github.com/yijing1998|branch'
+urepopath=$ufolder/${urepo%%|*}
+hfolder='hexofolder'
+
+check_git_repo()
+{
+	:
+}
 
 get_git_themes()
 {
-	for i in `echo ${tlist}`; do
+	for i in `echo ${trepos}`; do
 		arr=(${i//|/ })
-		if [ -d themes/${arr[0]} ]; then
-			rm -rf themes/${arr[0]}
-		fi
-		git clone ${arr[1]} themes/${arr[0]}
-		if [ $? -eq 0 ]; then
-			git checkout ${arr[2]}	
+		tar=$tfolder/${arr[0]}
+		#check theme repo folder's existance
+		if [ -d $tar ]; then
+			echo "Can't overwrite local folder: $tar, please remove it manually."
+		else
+			git clone ${arr[1]} $tar
+			if [ $? -eq 0 ]; then
+				cd $tar
+				git checkout ${arr[2]}
+				cd $rfolder
+			fi
 		fi
 	done
 }
 
-get_git_themes
+get_git_ufiles()
+{
+	arr=(${urepo//|/ })
+	tar=$ufolder/${arr[0]}
+	#check user repo folder's existance
+	if [ -d $tar ]; then
+		echo "Can't overwrite local folder: $tar, please remove it manually."
+	else
+		git clone ${arr[1]} $tar
+		if [ $? -eq 0 ]; then
+			cd $tar
+			git checkout ${arr[2]}
+			cd $rfolder
+		fi
+	fi
+}
 
-echo end it
+hexo_init()
+{
+	if [ -d $hfolder ]; then
+		echo "Can't init hexo in local folder: $hfolder, it's already exist, please remove it manually."
+	else
+		hexo init $hfolder
+	fi
+}
+
+link_things()
+{
+	rm -f $hfolder/_config.yml
+	rm -rf $hfolder/themes
+	rm -rf $hfolder/source
+	cd $hfolder
+	ln -s ../_config.yml _config.yml
+	ln -s ../$tfolder themes
+	ln -s ../$urepopath source
+	cd $rfolder
+}
+
+# try the recipe
+get_git_ufiles
+get_git_themes
+init_hexo
+link_things

924dcb2989a4228c57fadb346edcd1e9a2f57047
yijing1998 <yijing1998@126.com>
2017-07-09 22:00:46 +0800

    add some commands
    init
    new post

--- recipe.sh
+++ recipe.sh
@@ -79,8 +79,93 @@ link_things()
 	cd $rfolder
 }
 
-# try the recipe
-get_git_ufiles
-get_git_themes
-init_hexo
-link_things
+# init the working place from a fresh git clone
+init_all()
+{
+	get_git_ufiles
+	get_git_themes
+	init_hexo
+	link_things
+}
+
+new_post()
+{
+	# existance check
+	if [ ! -d $urepopath ]; then
+		echo No user repo files found! Try \'recipe git urepo\'.
+		return
+	fi
+
+	popath="$urepopath/_posts"
+	daystr=`date "+%Y-%m-%d"`
+
+  tmpcnt=`find $popath -maxdepth 1 -name "$daystr*" | wc -w`
+
+	pname=''
+	if [ $tmpcnt -eq 0 ]; then
+		pname="o01"
+	else
+		# find the max number and calculate the new post file name
+		tmpstr=`find $popath -maxdepth 1 -name "$daystr*" | sort -r | head -1 | grep -o "o[[:digit:]]\{2\}"`
+		mnum=${tmpstr#*o0}
+		if [ "$mnum" = "$tmpstr" ]; then
+			mnum=${tmpstr#*o}
+		fi
+		mnum=$[mnum+1]
+		echo $mnum;
+		if [ $mnum -lt 10 ]; then
+			pname="o0"${mnum}
+		elif [ $mnum -lt 100 ]; then
+			pname="o"${mnum}
+		else
+			echo "Too many posts a day! 99 is the max."
+			return
+		fi
+	fi
+
+	cd $hfolder
+	hexo new $pname
+	cd $rfolder
+}
+
+last_post()
+{
+	:
+}
+
+usage()
+{
+	echo 'Entering usage()'
+}
+
+# really do sth
+if [ $# -eq 1 ]; then
+	case $1 in
+		init )
+			# init
+			init_all
+			;;
+		* )
+			usage
+			;;
+	esac
+elif [ $# -eq 2 ]; then
+	case $1 in
+		new )
+			case $2 in
+				post )
+					# new post
+					new_post
+					;;
+				* )
+					usage
+					;;
+			esac
+			;;
+		* )
+			usage
+			;;
+	esac
+else
+	usage
+fi

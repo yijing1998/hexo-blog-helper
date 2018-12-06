75d675323220de4d438dd9b88be5e13fcc7ebac0
yijing1998 <yijing1998@126.com>
2018-12-06 22:31:30 +0800

    moveaction() need more

--- recipe.sh
+++ recipe.sh
@@ -33,7 +33,7 @@ while read MYLINE; do
     if [ "#" = "${MYLINE:0:1}" ]; then
         continue
     fi
-    if [ 0 -eq ${#MYLINE} ]; then
+    if [ -z $MYLINE ]; then
         continue
     fi
     eval ${MYLINE%%:*}=${MYLINE#*:}
@@ -67,7 +67,7 @@ do_new_draft()
     local fn=""
 	local tmpstr=`ls ${popath}${ymname} ${drpath}${ymname} 2> /dev/null | grep .*\.md | sort -r | head -1`
 
-    if [ 0 -eq ${#tmpstr} ]; then
+    if [ -z $tmpstr ]; then
 		# first post of current month
 		fn=`date "+%Y-%m-%d.001.md"`
     else
@@ -95,12 +95,56 @@ tags:
 ---
 $MYEOF
   echo "Info: New draft created! File Name: $fn"
-  echo "press 'Enter' to continue ..."
-  read
+  read -p "press 'Enter' to continue ..."
 }
 
-# move a draft to post
-do_draft2post()
+# check if $1 is number
+# and $1 is between $2 and $3
+# 0: is    1: no
+numrangeok()
+{
+    # param check
+    if [ ! $# -eq 3 ]; then
+        echo 1
+        return
+    fi
+    
+    # number check
+    local tmpstr=`echo $1$2$3 | grep '^[[:digit:]]*$'`
+    if [ -z $tmpstr ]; then
+        echo 1
+        return
+    fi
+
+    # range check
+    if [ $1 -lt $2 ]; then
+        # too small
+        echo 1
+        return
+    fi
+    if [ $1 -gt $3 ]; then
+        # too big
+        echo 1
+        return
+    fi
+
+    echo 0
+}
+
+# move filename $1 between $2 and $3
+# do not check params: the caller has checked them
+moveaction()
+{
+    echo `pwd` 1>&2
+    local mypath=`pathfix $(cd $CFGHEXOMYSOURCE; pwd)`
+    echo $mypath 1>&2
+
+    echo 0
+}
+
+# move a blog file between _draft and _post according to $1
+# 0: draft2post    1: post2draft
+do_movefile()
 {
     if [ ! -d "$CFGHEXOMYSOURCE" ]; then
         echo "Error: Please creat your blog folder in up menu => 'Mysource manage'." >&2
@@ -114,17 +158,101 @@ do_draft2post()
     local drpath="${mypath}_drafts/"
 	local popath="${mypath}_posts/"
     local farr=
+    local farrsize=0
     local farrvalid=0
-    local cpage=1
-    local pgsize=10
+    local cpage=0
+    local cid=0
+    local pgcnt=0
+    local tmpcnt=0
+    local cmin=0
+    local cmax=0
+    local nowfrom=
+    local nowto=
+    local nowmsg=
+
+    if [ $1 -eq 0 ]; then
+        nowfrom=$drpath
+        nowto=$popath
+        nowmsg="Move file from _draft to _post. Files are listed here.\nChoose the file NUMBER Or pageup=>j pagedown=>k back=>x"
+    else
+        nowfrom=$popath
+        nowto=$drpath
+        nowmsg="Move file from _post to _draft. Files are listed here.\nChoose the file NUMBER Or pageup=>j pagedown=>k back=>x"
+    fi
+
     while [ 0 ]; do
+        clear
+        echo -e $nowmsg 
         # get fname array
         if [ 0 -eq $farrvalid ]; then
-            farr=(`ls -Rrl $drpath | grep ^- | awk '{print $9}'`)
+            farr=(`ls -Rrl $nowfrom | grep ^- | awk '{print $9}'`)
+            ((farrsize=${#farr[*]}))
+            if [ 0 -eq $farrsize ]; then
+                echo "Info: No posts in folder _${nowfrom##*_}."
+                read -p "press 'Enter' to continue ..."
+                break
+            fi
             farrvalid=1
+            ((pgcnt=${farrsize}%${CFGPAGESIZE}))
+            if [ $pgcnt -gt 0 ]; then
+                ((pgcnt=${farrsize}/${CFGPAGESIZE}+1))
+            else
+                ((pgcnt=${farrsize}/${CFGPAGESIZE}))
+            fi
+            cpage=0
         fi
-        echo $farr ${#farr[*]}
-        break
+        
+        for ((i=0;i<$CFGPAGESIZE;i++)); do
+            ((cid=$i+$cpage*$CFGPAGESIZE))
+            if [ $cid -ge $farrsize ]; then
+                break
+            fi
+            echo $cid ${farr[$cid]}
+        done
+
+        ((cmin=$cpage*$CFGPAGESIZE))
+        ((cmax=$cmin+$CFGPAGESIZE-1))
+        ((tmpcnt=$farrsize-1))
+        if [ $cmax -gt $tmpcnt ]; then
+            cmax=$tmpcnt
+        fi
+
+        read -p "Your choice: " MYCMD
+        case $MYCMD in
+            x) break ;;
+            j) # prev page
+                if [ $cpage -gt 0 ]; then
+                    ((cpage-=1))
+                fi
+                ;;
+            k) # next page
+                ((tmpcnt=$pgcnt-1))
+                if [ $cpage -lt $tmpcnt ] ; then
+                    ((cpage+=1))
+                fi
+                ;;
+            *) # parse Num
+                if [ -z $MYCMD ]; then
+                    # no input
+                    continue
+                fi
+
+                if [ 1 -eq `numrangeok $MYCMD $cmin $cmax` ]; then
+                    # not pass the check
+                    continue
+                fi
+                if [ 1 -eq `moveaction ${farr[$MYCMD]} $nowfrom $nowto` ]; then
+                    # move failed
+                    echo "Move failed. Please check user permissions."
+                    read -p "press 'Enter' to continue ..."
+                    continue
+                fi
+                
+                echo "Move succeeded."
+                read -p "press 'Enter' to continue ..."
+                ((farrvalid=1))
+                ;;
+        esac
     done
 }
 
@@ -132,6 +260,7 @@ do_draft2post()
 menu_myblog()
 {
     while [ 0 ] ; do
+        clear
         echo "You can manage your draft or post here."
         select MYSEL in \
             "New draft" \
@@ -143,8 +272,8 @@ menu_myblog()
         ; do
             case $REPLY in
                 1) do_new_draft ;;
-                2) do_draft2post ;;
-                3) : ;;
+                2) do_movefile 0 ;;
+                3) do_movefile 1 ;;
                 4) : ;;
                 5) : ;;
                 6) : ;;
@@ -158,7 +287,7 @@ menu_myblog()
 }
 
 # test
-do_draft2post
+menu_myblog
 
 # Main menu
 : <<$MYEOF
